067ea5b5cfeb65d551e6a61398ed61659b51f9eb
Alex Miller <alex.k.miller@gmail.com>
2016-11-10 12:35:59 -0500

    Un-recursive due to max-depth and fewer requests to go fast!

--- app/main_app.py
+++ app/main_app.py
@@ -44,7 +44,7 @@ def valid_budget(date_str,value,period_start_str,period_end_str,currency,default
     
     return True
 
-def oipa_recursive_url_getter(url, filename, current_count=0, valid_count=0):
+def oipa_url_getter(url, filename):
     """
     High level method for extracting activity budgets from OIPA
 	Serialize to CSV file with 3 columns: iati-identifier, url, hash
@@ -53,61 +53,62 @@ def oipa_recursive_url_getter(url, filename, current_count=0, valid_count=0):
     
     # Build the request
     r = requests.get(url)
-
-    # extract activity URLs and store in the list
-    for activity in r.json()['results']:
-        current_count = current_count + 1
-        # Exclude automatically if all of the reporters are secondary
-        # Unfortunately, it's the only attribute we can exclude before delving deeper
-        secondary_flags = [org['secondary_reporter'] for org in activity['reporting_organisations']]
-        if sum(secondary_flags) == len(secondary_flags):
-            next
-            
-        #Exclude invalid IATI identifiers
-        try:
-            org_code = [org['organisation']['organisation_identifier'] for org in activity['reporting_organisations']][0]
-        except (KeyError,IndexError,TypeError):
-            org_code = ""
-        try:
-            identifier = activity['iati_identifier']
-        except (KeyError,IndexError,TypeError):
-            identifier = ""
-
-        if identifier[:len(org_code)]!=org_code:
-            next
-            
-        extraction = extract_activity(activity['url'])
-        if extraction:
-            valid_count = valid_count + 1
-            for budget_item in extraction:
-                budget_item['oipa-activity-url'] = activity['url']
-            df = pd.DataFrame(extraction)
-            now = datetime.now()
-            if valid_count==1:
-                df.to_csv(filename,index=False,sep="|")
-            else:
-                df.to_csv(filename,mode="a",header=False,index=False,sep="|")
-
-    total = r.json()['count']
-    sys.stdout.write('%d percent complete | %d of %d\r' % (current_count / total * 100.000, current_count, total))
-    sys.stdout.flush()
-
-    if r.json()['next'] == 'null':
-        return(True) # TODO: exit the loop and notify that the process has run
-
-    # if there are more pages, then recurse into the next page
-    else:
-        return(oipa_recursive_url_getter(r.json()['next'], filename, current_count, valid_count))
+    current_count=0
+    valid_count=0
     
-def extract_activity(url):
+    invalid_urls = ['null','None',None]
+    while r.json()['next'] not in invalid_urls:
+
+        # extract activity URLs and store in the list
+        for activity in r.json()['results']:
+            current_count = current_count + 1
+            # Exclude automatically if all of the reporters are secondary
+            # Unfortunately, it's the only attribute we can exclude before delving deeper
+            secondary_flags = [org['secondary_reporter'] for org in activity['reporting_organisations']]
+            if sum(secondary_flags) == len(secondary_flags):
+                next
+                
+            #Exclude invalid IATI identifiers
+            try:
+                org_code = [org['organisation']['organisation_identifier'] for org in activity['reporting_organisations']][0]
+            except (KeyError,IndexError,TypeError):
+                org_code = ""
+            try:
+                identifier = activity['iati_identifier']
+            except (KeyError,IndexError,TypeError):
+                identifier = ""
+    
+            if identifier[:len(org_code)]!=org_code:
+                next
+                
+            extraction = extract_activity(activity)
+            
+            if extraction:
+                valid_count = valid_count + 1
+                for budget_item in extraction:
+                    budget_item['oipa-activity-url'] = activity['url']
+                df = pd.DataFrame(extraction)
+                now = datetime.now()
+                if valid_count==1:
+                    df.to_csv(filename,index=False,sep="|")
+                else:
+                    df.to_csv(filename,mode="a",header=False,index=False,sep="|")
+    
+        total = r.json()['count']
+        sys.stdout.write('%d percent complete | %d of %d\r' % (current_count / total * 100.000, current_count, total))
+        sys.stdout.flush()
+        
+        r = requests.get(r.json()['next'])
+
+    return(True) # TODO: exit the loop and notify that the process has run
+
+    
+def extract_activity(activity):
     """
     Method for returning attributes given activity URL
     """
 
-    # Build the request
-    r = requests.get(url)
-    activity = r.json()
-
+    dates = {}
     if len(activity['activity_dates'])>0:
         planned_start = [date['iso_date'] for date in activity['activity_dates'] if date['type']['code']=='1']
         actual_start = [date['iso_date'] for date in activity['activity_dates'] if date['type']['code']=='2']
@@ -115,7 +116,6 @@ def extract_activity(url):
         actual_end = [date['iso_date'] for date in activity['activity_dates'] if date['type']['code']=='4']
             
         #Parse any available dates, ensuring real ISO
-        dates = {}
         if len(planned_start)>0:
             try:
                 parsed_planned_start = datetime.strptime(planned_start[0],'%Y-%m-%d')
--- app/run.py
+++ app/run.py
@@ -1,10 +1,18 @@
 from main_app import *
 from datetime import datetime
 
-base_url = 'https://www.oipa.nl/api/activities/?format=json'
+base_url = 'https://www.oipa.nl/api/activities/?format=json&fields=budgets,reporting_organisations,iati_identifier,url,activity_dates,aggregations,title,participating_organisations,recipient_countries,recipient_regions,sectors,policy_markers,collaboration_type,default_flow_type,default_finance_type,default_aid_type,default_tied_status&total_budget_value_gte=1&reporting_organisation='
+
+reporters = [
+    # "XM-DAC-41114",
+    "GB-CHC-207544"
+]
+for reporter in reporters:
+    base_url = base_url + reporter + ","
 
 filename = "output_"+datetime.now().strftime("%Y_%m_%d")+".csv"
 
-oipa_recursive_url_getter(base_url,filename)
+oipa_url_getter(base_url,filename)
 
+print("\n")
 print("Done.")
